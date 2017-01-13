2fdc79312c3f797a7be0de4f2cea74c54f9b80c2
sunilrajramchandani <sunilraj.ramchandani@benilde.edu.ph>
2017-01-13 11:26:17 +0800

    html to php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Archie Arintok</title>
+		<meta keyword="Sunil Raj" vaue="Archie Arintok"/>
+		<meta keyword="description" value="WEBDEVT Exercise #1"/>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+		integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="navbar.css">
+		<link rel="stylesheet" type="text/css" href="csssss.css">
+</head>
+<body>
+<?php include('nabar.php'); ?>
+	<div class="container">
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+		</div>
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+			<p class="h1 text-center"><strong>ARCHIE ARINTOK</strong></p>
+			<p class="h3 text-center text-muted">The Heartbreak Kid</p>
+			<img src='archie.png' class="img-responsive">			
+		</div>
+		<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+		</div>
+	</div>
+</body>
+</html>
+
