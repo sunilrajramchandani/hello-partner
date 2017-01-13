6d0c7aeaa02e6a561820753e4d866211f19875f4
sunilrajramchandani <sunilraj.ramchandani@benilde.edu.ph>
2017-01-13 11:27:05 +0800

    new navbar w/ css

---
+++ nabar.php
@@ -0,0 +1,31 @@
+
+
+<nav class="navbar navbar-default" role = "navigation">
+		<div class="navbar-header">
+			<div class="container-fluid">
+				<div class="col-lg-4 col-md-4 col-sm-4  col-xs-4">
+						<span class = "glyphicon-envelope"></span>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-center">
+					<p class = "h3 text-center">IVANN ARCHIE ARINTOK</p>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+						<button type = "button" class = "navbar-toggle" data-toggle = "collapse" data-target = "#example-navbar-collapse">
+						 <span class = "sr-only">Toggle navigation</span>
+						 <span class = "icon-bar"></span>
+						 <span class = "icon-bar"></span>
+						 <span class = "icon-bar"></span>
+					</button>
+					<div class = "collapse navbar-collapse" id = "example-navbar-collapse">
+						<ul class = "nav navbar-nav">
+							<li class="active"><a href = "#">Home</a></li>
+							<li><a href = "#">About</a></li>
+							<li><a href = "#">Top 5</a></li>	
+						</ul>
+					</div>		
+				</div>
+			</div>
+		</div>	
+</nav>
+
+         
